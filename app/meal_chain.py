20ce11c176531151145a8c2b7b4007ab40302477
VIKAS RAI <vikasraivsr630@gmail.com>
2026-09-24 14:58:36 +0530

    Update ChatPromptTemplate import

--- app/meal_chain.py
+++ app/meal_chain.py
@@ -1,4 +1,4 @@
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable
 from langchain_google_genai import ChatGoogleGenerativeAI
 from sqlalchemy.orm import Session
